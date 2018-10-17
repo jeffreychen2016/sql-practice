fc07a208d040a6c0631305655050f12b5b70bdce
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-10-16 20:13:23 -0500

    finished 13

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -78,9 +78,22 @@
 --GROUP BY InvoiceId
 
 --12. Provide a query that includes the purchased track name with each invoice line item.
+--SELECT
+-- Track.Name
+-- ,InvoiceLine.InvoiceId
+--FROM InvoiceLine
+--INNER JOIN Track
+--ON InvoiceLine.TrackId = Track.TrackId
+
+--13. Provide a query that includes the purchased track name AND artist name with each invoice line item.
 SELECT
- Track.Name
+ TrackName = Track.Name
  ,InvoiceLine.InvoiceId
+ ,Artist = Artist.Name
 FROM InvoiceLine
 INNER JOIN Track
 ON InvoiceLine.TrackId = Track.TrackId
+INNER JOIN Album
+ON Track.AlbumId = Album.AlbumId
+INNER JOIN Artist
+ON Album.ArtistId = Artist.ArtistId
